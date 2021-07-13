bbc11a509d650bf35b854384d94a9c5b52a1819a
manny-m <mmoquete@edx.org>
2021-07-13 09:26:03 -0400

    admin index

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import qs from 'query-string';
-import { Icon } from '@edx/paragon';
+import { Icon, Alert } from '@edx/paragon';
+import { Info as InfoIcon } from '@edx/paragon/icons';
 import { Link } from 'react-router-dom';
 
 import Hero from '../Hero';
-import StatusAlert from '../StatusAlert';
 import EnrollmentsTable from '../EnrollmentsTable';
 import RegisteredLearnersTable from '../RegisteredLearnersTable';
 import EnrolledLearnersTable from '../EnrolledLearnersTable';
@@ -249,24 +249,25 @@ class Admin extends React.Component {
 
   renderErrorMessage() {
     return (
-      <StatusAlert
-        alertType="danger"
-        iconClassName="fa fa-times-circle"
-        title="Unable to load overview"
-        message={`Try refreshing your screen (${this.props.error.message})`}
-      />
+      <Alert
+        variant="danger"
+        icon={InfoIcon}
+      >
+        <Alert.Heading>Unable to load overview</Alert.Heading>
+        {`Try refreshing your screen (${this.props.error.message})`}
+      </Alert>
     );
   }
 
   renderCsvErrorMessage(message) {
     return (
-      <StatusAlert
-        className="mt-3"
-        alertType="danger"
-        iconClassName="fa fa-times-circle"
-        title="Unable to Generate CSV Report"
-        message={`Please try again. (${message})`}
-      />
+      <Alert
+        variant="danger"
+        icon={InfoIcon}
+      >
+        <Alert.Heading>Unable to Generate CSV Report</Alert.Heading>
+        {`Please try again. (${message})`}
+      </Alert>
     );
   }
 
