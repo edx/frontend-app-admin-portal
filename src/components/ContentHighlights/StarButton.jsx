ebfc6519f8bfd343d56db4971ae425c49b48783b
Srija Indukuri <ssrijaindukuri-sonata@2u.com>
2026-07-14 15:34:36 +0000

    fix: align cards and prevent checkbox and star overflow

--- src/components/ContentHighlights/StarButton.jsx
+++ src/components/ContentHighlights/StarButton.jsx
@@ -38,9 +38,6 @@ const StarButton = ({
       data-testid={`star-btn-${uuid}`}
       className="star-btn p-0 border-0 bg-transparent shadow-none"
       style={{
-        position: 'absolute',
-        top: '1px',
-        right: '-25px',
         width: '24px',
         height: '24px',
       }}
